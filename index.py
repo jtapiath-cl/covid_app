d455f883978739dcf42727fda54f98ca18f2d714
Javier Tapia <javier@ctjconsult.com>
2020-09-15 01:24:22 +0000

    Final version 2.0 published and deployed

--- index.py
+++ index.py
@@ -60,7 +60,7 @@ def toggle_collapse(n, is_open):
     return is_open
 
 if __name__ == "__main__":
-    app.run_server(debug = True, 
+    app.run_server(debug = False, 
         host = "0.0.0.0", 
         port = 8050,
         dev_tools_hot_reload = False
